81a6c288613068695aeddf4c899c1b6773875d21
Jodndud <doriconi@gmail.com>
2025-10-01 17:52:36 +0900

    프로젝트 생성, 라우트 설정 완료

---
+++ first-step-town/src/App.tsx
@@ -0,0 +1,20 @@
+﻿import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Stock from "./pages/Game/Stock";
+import Typing from "./pages/Game/Typing";
+import Calculating from "./pages/Game/Calculating";
+
+export default function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/game">
+          <Route path="stock" element={<Stock />} />
+          <Route path="styping" element={<Typing />} />
+          <Route path="calculating" element={<Calculating />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
+}
---
+++ first-step-town/src/pages/Game/Calculating.tsx
@@ -0,0 +1,3 @@
+export default function Calculating() {
+  return <div>Calculating</div>;
+}
